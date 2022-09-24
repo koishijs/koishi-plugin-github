a82a0c1c9816efd59299e7c38ea7f21655b493a6
Shigma <1700011071@pku.edu.cn>
2022-09-25 00:09:07 +0800

    test: refactor test fixtures and snapshots

--- tests/events.spec.ts
+++ tests/events.spec.ts
@@ -17,7 +17,19 @@ app.plugin(mock)
 
 const client = app.mock.client('123', '999')
 
-const snapshot = require('./index.snap')
+const snapshots = require('./snapshots')
+
+function* listFixtures(cwd: string, prefix = ''): Generator<string> {
+  const dirents = readdirSync(cwd, { withFileTypes: true })
+  for (const dirent of dirents) {
+    const name = resolve(cwd, dirent.name)
+    if (dirent.isDirectory()) {
+      yield* listFixtures(resolve(cwd, dirent.name), prefix + dirent.name + '/')
+    } else if (dirent.isFile() && name.endsWith('.json')) {
+      yield prefix + dirent.name.slice(0, -5)
+    }
+  }
+}
 
 describe('koishi-plugin-github (events)', () => {
   before(async () => {
@@ -46,9 +58,7 @@ describe('koishi-plugin-github (events)', () => {
     // spy on sendMessage
     const sendMessage = app.bots[0].sendMessage = jest.fn<Bot['sendMessage']>()
 
-    const files = readdirSync(resolve(__dirname, 'fixtures'))
-    files.forEach((file) => {
-      const title = file.slice(0, -5)
+    for (const title of listFixtures(resolve(__dirname, 'fixtures'))) {
       it(title, async () => {
         sendMessage.mockClear()
         sendMessage.mockImplementation(() => {
@@ -56,7 +66,7 @@ describe('koishi-plugin-github (events)', () => {
         })
 
         const payload = require(`./fixtures/${title}`)
-        const [event] = title.split('.', 1)
+        const event = title.split(title.includes('/') ? '/' : '.', 1)
         const fullEvent = payload.action ? `${event}/${payload.action}` : event
         if (payload.action) {
           app.emit(`github/${fullEvent}` as any, payload)
@@ -65,70 +75,70 @@ describe('koishi-plugin-github (events)', () => {
 
         // wait until all messages are sent
         await sleep(0)
-        if (snapshot[title]) {
+        if (snapshots[title]) {
           expect(sendMessage.mock.calls).to.have.length(1)
-          expect(sendMessage.mock.calls[0][1]).to.equal(snapshot[title].trim())
+          expect(sendMessage.mock.calls[0][1]).to.equal(snapshots[title].trim())
         } else {
           expect(sendMessage.mock.calls).to.have.length(0)
         }
       })
-    })
+    }
   })
 
   describe('Quick Interactions', () => {
     it('no operation', async () => {
-      await client.shouldNotReply(`<quote id="${idMap['issue_comment.created.1']}"/>`)
-      await client.shouldNotReply(`<quote id="${idMap['issue_comment.created.1']}"/> .noop`)
+      await client.shouldNotReply(`<quote id="${idMap['issue_comment/created.1']}"/>`)
+      await client.shouldNotReply(`<quote id="${idMap['issue_comment/created.1']}"/> .noop`)
     })
 
     it('link', async () => {
       await client.shouldReply(
-        `<quote id=${idMap['issue_comment.created.1']}/> .link`,
+        `<quote id=${idMap['issue_comment/created.1']}/> .link`,
         'https://github.com/koishijs/koishi/issues/19#issuecomment-576277946',
       )
     })
 
     it('react', async () => {
       const reaction = mockResponse('POST', '/issues/comments/576277946/reactions', [200])
-      await client.shouldNotReply(`<quote id=${idMap['issue_comment.created.1']}/> laugh`)
+      await client.shouldNotReply(`<quote id=${idMap['issue_comment/created.1']}/> laugh`)
       expect(reaction.mock.calls).to.have.length(1)
     })
 
     it('reply', async () => {
       const comment = mockResponse('POST', '/issues/19/comments', [200])
-      await client.shouldNotReply(`<quote id=${idMap['issue_comment.created.1']}/> test`)
+      await client.shouldNotReply(`<quote id=${idMap['issue_comment/created.1']}/> test`)
       expect(comment.mock.calls).to.have.length(1)
     })
 
     it('close', async () => {
       const api1 = mockResponse('PATCH', '/issues/20', [200])
       const api2 = mockResponse('POST', '/issues/20/comments', [200])
-      await client.shouldNotReply(`<quote id=${idMap['pull_request.opened.1']}/> .close foo`)
+      await client.shouldNotReply(`<quote id=${idMap['pull_request/opened.1']}/> .close foo`)
       expect(api1.mock.calls).to.have.length(1)
       expect(api2.mock.calls).to.have.length(1)
     })
 
     it('base', async () => {
       const api = mockResponse('PATCH', '/pulls/20', [200])
-      await client.shouldNotReply(`<quote id=${idMap['pull_request.opened.1']}/> .base foo`)
+      await client.shouldNotReply(`<quote id=${idMap['pull_request/opened.1']}/> .base foo`)
       expect(api.mock.calls).to.have.length(1)
     })
 
     it('merge', async () => {
       const api = mockResponse('PUT', '/pulls/20/merge', [200])
-      await client.shouldNotReply(`<quote id=${idMap['pull_request.opened.1']}/> .merge`)
+      await client.shouldNotReply(`<quote id=${idMap['pull_request/opened.1']}/> .merge`)
       expect(api.mock.calls).to.have.length(1)
     })
 
     it('rebase', async () => {
       const api = mockResponse('PUT', '/pulls/20/merge', [200])
-      await client.shouldNotReply(`<quote id=${idMap['pull_request.opened.1']}/> .rebase`)
+      await client.shouldNotReply(`<quote id=${idMap['pull_request/opened.1']}/> .rebase`)
       expect(api.mock.calls).to.have.length(1)
     })
 
     it('squash', async () => {
       const api = mockResponse('PUT', '/pulls/20/merge', [200])
-      await client.shouldNotReply(`<quote id=${idMap['pull_request.opened.1']}/> .squash`)
+      await client.shouldNotReply(`<quote id=${idMap['pull_request/opened.1']}/> .squash`)
       expect(api.mock.calls).to.have.length(1)
     })
   })
