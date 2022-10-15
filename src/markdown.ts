55ecf3adcac7026de732fdff41f4232c715eed68
Shigma <shigma10826@gmail.com>
2022-10-16 01:32:29 +0800

    feat: support config replyFooter, fix #10

--- src/markdown.ts
+++ src/markdown.ts
@@ -30,8 +30,12 @@ function render(tokens: marked.Token[]) {
   return tokens.map(renderToken).join('')
 }
 
+export const INDICATOR = '<!-- BOT-MESSAGE-FOOTER -->'
+
 export function transform(source: string) {
   if (!source) return ''
+  const index = source.indexOf(INDICATOR)
+  if (index >= 0) source = source.slice(0, index)
   source = source.replace(/^<!--(.*)-->$/gm, '')
   return render(marked.lexer(source)).trim().replace(/\n\s*\n/g, '\n')
 }
