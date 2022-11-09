63e6056161cd61fdd3c39fb09a2b6641d1b8eb00
Shigma <shigma10826@gmail.com>
2022-11-09 20:38:19 +0800

    docs: migrate to @koishijs/vitepress

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,19 +1,26 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from '@koishijs/vitepress'
 
 export default defineConfig({
   lang: 'zh-CN',
   title: 'koishi-plugin-github',
   description: 'GitHub Toolkit for Koishi',
+
   themeConfig: {
-    outline: [2, 3],
     sidebar: [{
       text: '指南',
       items: [
         { text: '介绍', link: './' },
         { text: '配置项', link: './config' },
       ],
+    }, {
+      text: '更多',
+      items: [
+        { text: 'Koishi 官网', link: 'https://koishi.chat' },
+        { text: '支持作者', link: 'https://afdian.net/a/shigma' },
+      ],
     }],
   },
+
   vite: {
     resolve: {
       dedupe: ['vue'],
