69ace1601e15ec911a5b8b13c7a64d0c3c30559f
Shigma <shigma10826@gmail.com>
2023-03-07 00:15:20 +0800

    feat: use koishi-plugin-markdown, fix #27

--- src/markdown.ts
+++ src/markdown.ts
@@ -1,34 +1,4 @@
-import { segment } from 'koishi'
-import { marked } from 'marked'
-
-declare module 'marked' {
-  namespace Tokens {
-    interface Def {
-      type: 'def'
-    }
-
-    interface Paragraph {
-      tokens: marked.Token[]
-    }
-  }
-}
-
-function renderToken(token: marked.Token) {
-  if (token.type === 'code') {
-    return token.text + '\n'
-  } else if (token.type === 'paragraph') {
-    return render(token.tokens)
-  } else if (token.type === 'image') {
-    return segment.image(token.href)
-  } else if (token.type === 'blockquote') {
-    return token.text
-  }
-  return token.raw
-}
-
-function render(tokens: marked.Token[]) {
-  return tokens.map(renderToken).join('')
-}
+import { md } from 'koishi-plugin-markdown'
 
 export const INDICATOR = '<!-- BOT-MESSAGE-FOOTER -->'
 
@@ -37,5 +7,5 @@ export function transform(source: string) {
   const index = source.indexOf(INDICATOR)
   if (index >= 0) source = source.slice(0, index)
   source = source.replace(/^<!--(.*)-->$/gm, '')
-  return render(marked.lexer(source)).trim().replace(/\n\s*\n/g, '\n')
+  return md(source).join('').trim().replace(/\n\s*\n/g, '\n')
 }
